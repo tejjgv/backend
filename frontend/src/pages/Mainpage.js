ecdf59452fd4b174d09b3d04d56d884ee61169eb
tejjgv <143538529+tejjgv@users.noreply.github.com>
2023-11-18 15:25:37 +0530

    Update Mainpage.js

--- frontend/src/pages/Mainpage.js
+++ frontend/src/pages/Mainpage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, Text, Link } from "@chakra-ui/react";
 import Navbar from "../components/others/navbar";
 
 const Mainpage = () => {
@@ -10,23 +10,40 @@ const Mainpage = () => {
         minH="100vh"
         display="flex"
         justifyContent="center"
+        alignItems="center"
         textAlign="center"
         flexDirection="column"
         bgGradient="linear(to-b, teal.200, teal.400)"
         mx={{ base: "0", md: "auto" }}
-        p={4}
+        p={8}
+        borderRadius="md"
+        boxShadow="md"
+        color="white"
       >
         <Heading
           as="h1"
           fontSize={{ base: "4xl", md: "5xl" }}
-          color="white"
           mb={4}
         >
           Welcome to the Language Learning Quiz App!
         </Heading>
-        <Text fontSize={{ base: "xl", md: "2xl" }} color="white" mb={8}>
-          Improve your language skills through fun quizzes.
+        <Text fontSize={{ base: "xl", md: "2xl" }} mb={8}>
+          Enhance your language skills through engaging quizzes.
         </Text>
+        <Link
+          href="https://tejjgv.github.io/app/"
+          target="_blank"
+          fontSize={{ base: "xl", md: "2xl" }}
+          bg="white"
+          color="teal.500"
+          px={4}
+          py={2}
+          borderRadius="md"
+          _hover={{ textDecoration: "none", bg: "teal.600" }}
+        >
+          Let's Learn a New Language
+        </Link>
+       
       </Box>
     </>
   );
